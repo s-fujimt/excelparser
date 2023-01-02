b9829625ca1bbf4a08f4f28d319a5a1a9e37613f
Mochikibi Web <mochikibiweb@gmail.com>
2023-01-02 12:19:35 +0900

    hmpf

--- api.py
+++ api.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from libs.excel_parser import ExcelParser
+import time
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/parse', methods=['POST'])
 def parse():
+    start_time = time.time()
+
     excel_file = request.files['file']
     excel_parser = ExcelParser()
     try:
         result_json = excel_parser.parse_xlsx_to_json_file(excel_file)
+        print("--- %s seconds ---" % (time.time() - start_time))
         if result_json:
             return result_json
         else:
--- libs/excel_parser.py
+++ libs/excel_parser.py
@@ -10,6 +10,8 @@ class ExcelParser:
     custom_index  = None
     current_sheet = None
     current_range = None
+    empty_rows = 0
+    empty_columns = 0
 
     def __get_merged_ranges(self):
         return self.current_sheet.merged_cells.ranges if self.current_sheet.merged_cells else []
@@ -129,11 +131,32 @@ class ExcelParser:
             return "double"
         else:
           return "single"
+
+    def __set_border(self, cell, border, direction, partner):
+        if getattr(border, direction) and getattr(border, direction).style and getattr(border, direction).color:
+            locals()[f"border_{direction}"].append(self.__get_border_style(getattr(border, direction).style))
+            locals()[f"border_{direction}"].append(self.__get_color_data(getattr(border, direction).color))
+        else:
+            if cell.row == 1 and direction == "top":
+                return False
+            if cell.column == 1 and direction == "left":
+                return False
+            if direction == "top":
+                neighbor = self.current_sheet.cell(row=cell.row-1, column=cell.column)
+            if direction == "right":
+                neighbor = self.current_sheet.cell(row=cell.row, column=cell.column+1)
+            if direction == "bottom":
+                neighbor = self.current_sheet.cell(row=cell.row+1, column=cell.column)
+            if direction == "left":
+                neighbor = self.current_sheet.cell(row=cell.row, column=cell.column-1)
+            if getattr(neighbor.border, partner) and getattr(neighbor.border, partner).style and getattr(neighbor.border, partner).color:
+                locals()[f"border_{direction}"].append(self.__get_border_style(getattr(neighbor.border, partner).style))
+                locals()[f"border_{direction}"].append(self.__get_color_data(getattr(neighbor.border, partner).color))
+        return locals()[f"border_{direction}"]
     
     def __get_cell_border_data(self, cell, is_merged_cell):
         try:
-            cell_border_data = {}
-            outline = {}
+            cell_border_data, outline = {}, {}
 
             border = cell.border
 
@@ -147,35 +170,7 @@ class ExcelParser:
                     if border_color:
                         outline["color"] = self.__get_color_data(border_color)
             else:
-                border_top = []
-                border_right = []
-                border_bottom = []
-                border_left = []
-
-                
-
-
-                for direction, partner in {"top": "bottom", "right": "left", "bottom": "top", "left": "right"}.items():
-                    if getattr(border, direction) and getattr(border, direction).style and getattr(border, direction).color:
-                        locals()[f"border_{direction}"].append(self.__get_border_style(getattr(border, direction).style))
-                        locals()[f"border_{direction}"].append(self.__get_color_data(getattr(border, direction).color))
-                    else:
-                        if cell.row == 1 and direction == "top":
-                            continue
-                        if cell.column == 1 and direction == "left":
-                            continue
-                        if direction == "top":
-                            neighbor = self.current_sheet.cell(row=cell.row-1, column=cell.column)
-                        if direction == "right":
-                            neighbor = self.current_sheet.cell(row=cell.row, column=cell.column+1)
-                        if direction == "bottom":
-                            neighbor = self.current_sheet.cell(row=cell.row+1, column=cell.column)
-                        if direction == "left":
-                            neighbor = self.current_sheet.cell(row=cell.row, column=cell.column-1)
-                        if getattr(neighbor.border, partner) and getattr(neighbor.border, partner).style and getattr(neighbor.border, partner).color:
-                            locals()[f"border_{direction}"].append(self.__get_border_style(getattr(neighbor.border, partner).style))
-                            locals()[f"border_{direction}"].append(self.__get_color_data(getattr(neighbor.border, partner).color))
-
+                [border_top, border_right, border_bottom, border_left]=[self.__set_border(cell, border, direction, partner) for direction, partner in {"top": "bottom", "right": "left", "bottom": "top", "left": "right"}.items() 
 
                 if border_top == border_right == border_bottom == border_left and len(border_top) > 0:
                     border = border_top
@@ -219,18 +214,13 @@ class ExcelParser:
             
             value = cell.value
             if value != None:
-                if cell.is_date:
-                    date = cell.value.strftime("%Y/%m/%d")
-                    cell_data["value"] = date
-                else:
-                    cell_data["value"] = value
+                cell_data["value"] = cell.value.strftime("%Y/%m/%d") if cell.is_date else value
 
             is_merged_cell = self.__is_merged_cell(cell, self.__get_merged_ranges())
             if is_merged_cell:
-                if cell.coordinate in self.__get_first_cells_of_merged_ranges():
-                    cell_data.update(self.__get_merged_cell_data())
-                else:
+                if cell.coordinate not in self.__get_first_cells_of_merged_ranges():
                     return {}
+                cell_data.update(self.__get_merged_cell_data())
 
             alignment = self.__get_cell_alignment(cell)
             if alignment:
@@ -248,61 +238,46 @@ class ExcelParser:
             if fill_color:
                 cell_data["fill"] = {"color":fill_color}
             
-            if cell_data.get("value") or cell_data.get("border") or cell_data.get("fill"):
-                return cell_data
-            else:
+            if not cell_data.get("value") or not cell_data.get("border") or not cell_data.get("fill"):
                 return {}
+
+            return cell_data
         except:
             print("Error getting cell data (" + cell.coordinate + ")")
             print(sys.exc_info()[0])
             return {}
 
+    def __get_cell_data_wrapper(self, cell):
+        cell_data = self.__get_cell_data(cell)
+        self.empty_columns = self.empty_columns + 1 if cell_data == {} else 0
+        return cell_data
+
     def __get_row_data(self, row):
-        empty_cells = 0
+        self.empty_columns = 0
         try:
             row_data = {
                 "linenumber": row[0].row
             }
 
-            columns = []
-            for cell in row:
-                if empty_cells == 50:
-                    columns = columns[:-50]
-                    break
-                if cell.column == self.current_sheet.max_column+1:
-                    break
-                cell_data = self.__get_cell_data(cell)
-                if cell_data:
-                    columns.append(cell_data)
-                    empty_cells = 0
-                else:
-                    empty_cells += 1
+            columns = [self.__get_cell_data_wrapper(cell) for cell in row if self.empty_columns < 50 and cell.column < self.current_sheet.max_column+2]
             if columns:
                 row_data["columns"] = columns
+
             return row_data
         except:
             print("Error getting row data")
             return {}
 
+    def __get_row_data_wrapper(self, row):
+        row_data = self.__get_row_data(row)
+        self.empty_rows = self.empty_rows + 1 if row_data.get('columns') is None or row_data == {} else 0
+        return row_data
+
+
     def __get_rows(self):
-        empty_rows = 0
+        self.empty_rows = 0
         try:
-            rows = []
-            for row in self.current_sheet.iter_rows():
-                if empty_rows == 50:
-                    rows = rows[:-50]
-                    break
-                if row[0].row == self.current_sheet.max_row+2:
-                    break
-                row_data = self.__get_row_data(row)
-                if row_data:
-                    rows.append(row_data)
-                    if row_data.get('columns'):
-                        empty_rows = 0
-                    else:
-                        empty_rows += 1
-                else:
-                    empty_rows += 1
+            rows = [self.__get_row_data_wrapper(row) for row in self.current_sheet.iter_rows() if self.empty_rows < 50 and row[0].row < self.current_sheet.max_row+2]
             return rows
         except:
             print("Error getting rows")
@@ -320,26 +295,22 @@ class ExcelParser:
                 "font": font_data,
                 "lines": rows
             }
-            if sheet_data:
-                return sheet_data
-            else:
-                return {}
+
+            return sheet_data
         except:
             print("Error getting sheet data")
             return {}
+
+    def __set_custom_index(self, color):
+        self.custom_index = [rgb.attrib['rgb'] for rgb in color]
 
     def __check_for_custom_index(self, filepath):
         try:
             with zipfile.ZipFile(filepath) as zgood:
                 styles_xml = zgood.read('xl/styles.xml')
                 root = ET.fromstring(styles_xml)
-                for child in root:
-                    if "colors" in child.tag:
-                        for color in child:
-                            if 'indexedColors' in color.tag:
-                                self.custom_index = []
-                                for rgb in color:
-                                    self.custom_index.append(rgb.attrib['rgb'])
+                [[self.__set_custom_index(color) for color in child if 'indexedColors' in color.tag] for child in root if 'colors' in child.tag]
+ 
         except:
             print("Error checking for custom index")
             return None
@@ -351,6 +322,10 @@ class ExcelParser:
         except:
             print("Error opening workbook")
             return None
+
+    def __get_sheet(self, i, sheetname):
+        self.current_sheet = self.workbook[sheetname]
+        return self.__get_sheet_data(i+1)
     
     def parse_xlsx_to_json_file(self, excel_path):
         try:
@@ -358,10 +333,7 @@ class ExcelParser:
             self.__check_for_custom_index(excel_path)
             sheet_names = self.workbook.sheetnames
 
-            sheet_data = []
-            for i, sheetname in enumerate(sheet_names):
-                self.current_sheet = self.workbook[sheetname]
-                sheet_data.append(self.__get_sheet_data(i+1))
+            sheet_data = [self.__get_sheet(i, sheetname) for i, sheetname in enumerate(sheet_names)]
 
             return json.dumps({"sheets": sheet_data}, ensure_ascii=False)
         except Exception as e:
